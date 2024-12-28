21b518b853d60c3f5007bce637846e79560e56de
Darcy Boddey <db533@student.le.ac.uk>
2024-12-28 22:44:39 +0000

    added @GetMapping for bakery, bread and ingredient

--- src/main/java/co2123/hw2/controller/ListController.java
+++ src/main/java/co2123/hw2/controller/ListController.java
@@ -1,8 +1,28 @@
 package co2123.hw2.controller;
 
+import co2123.hw2.model.Bakery;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ListController {
+    @GetMapping(value= "/listBakery")
+    public String listBakery(Model model) {
+        model.addAttribute("data", "bakery");
+        return "list";
+    }
+
+    @GetMapping(value= "/listBread")
+    public String listBread(Model model) {
+        model.addAttribute("data", "bread");
+        return "list";
+    }
+
+    @GetMapping(value= "/listIngredient")
+    public String listIngredient(Model model) {
+        model.addAttribute("data", "ingredient");
+        return "list";
+    }
 
 }
